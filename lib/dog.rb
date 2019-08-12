10f6fb71d17c860ae334056945adec9be81e832d
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-12 03:27:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,14 +2,4 @@ class Dog
   def initialize(name)
     @name = name
   end
-  def initialize(breed)
-    @breed = breed
-  end
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
 end
